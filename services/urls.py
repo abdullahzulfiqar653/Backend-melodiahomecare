d03d0dbf76c7c205862507d94565f2091ca62875
anmirza786 <anmirza360@gmail.com>
2021-08-31 16:05:17 +0500

    updation.

--- services/urls.py
+++ services/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path('home', views.home, name='home'),
     path('nursing-service', views.nursing_service, name='nursing_service'),
     path('home-care-nursing', views.home_care, name='home_care'),
+    path('home-health-aide', views.health_aide, name='health_aide'),
+    path('home-making-services', views.home_making, name='home_making'),
+    path('personal-care-assistant', views.personal_assistant,name='personal_assistant'),
 ]
--- services/views.py
+++ services/views.py
@@ -1,6 +1,6 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
-from .models import Nursing_Service, Care_Service
+from .models import Nursing_Service, Care_Service, Aid_Service, Homemaking_Service,Assistant_Service
 # Create your views here.
 
 
@@ -16,3 +16,16 @@ def nursing_service(request):
 def home_care(request):
     home_car = Care_Service.objects.all()
     return render(request, 'home-care-nursing.html', {'service': home_car})
+
+
+def health_aide(request):
+    health_aid = Aid_Service.objects.all()
+    return render(request, 'home-health-aide.html', {'service': health_aid})
+
+def personal_assistant(request):
+    assistant = Assistant_Service.objects.all()
+    return render(request, 'personal-care-assistant.html', {'service': assistant})
+
+def home_making(request):
+    homemaking = Homemaking_Service.objects.all()
+    return render(request, 'homemaking-services.html', {'service': home_making})
